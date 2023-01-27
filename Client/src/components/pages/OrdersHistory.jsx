21f0fc3ec93e8bf8a4772984abbb8c30a8c27561
EdenItz <62380541+EdenItz@users.noreply.github.com>
2023-01-27 22:16:44 +0200

    add pie chart for products categories

--- Client/src/components/pages/OrdersHistory.jsx
+++ Client/src/components/pages/OrdersHistory.jsx
@@ -1,89 +1,159 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Accordion, AccordionDetails, AccordionSummary, Button} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import Typography from '@mui/material/Typography'
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Button,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
 import { useCookies } from 'react-cookie';
 
+import CategoryPieChart from '../CategoryPieChart.js';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import ProductOrder from "../ProductOrder";
+import ProductOrder from '../ProductOrder';
 import { getHistory } from '../../services/orderService';
-import { errorMsg, successMsg } from '../../services/feedbackService';
+import { errorMsg } from '../../services/feedbackService';
 import { UserContext } from '../../App';
 
-import '../../css/OrdersHistory.css'
+import '../../css/OrdersHistory.css';
 
 function OrdersHistory({}) {
     const [loading, setLoading] = useState(false);
     const [orders, setOrders] = useState([]);
+    const [categoryData, setCategoryData] = useState([]);
     const [expandedOrder, setExpandedOrder] = useState();
     const [cookies] = useCookies();
     const userDetails = useContext(UserContext);
 
     useEffect(() => {
         const fetchOrders = async () => {
-            setLoading(true)  
-            
-            if (!cookies.eShopToken){
-                errorMsg('Please login and try again.')
-                return setLoading(false)
+            let categoryStats = [];
+            const addToCat = (category, quantity) => {
+                if (quantity <= 0 || !category) return;
+
+                const currCatIndex = categoryStats.findIndex(
+                    cat => cat.label === category,
+                );
+
+                if (categoryStats[currCatIndex])
+                    categoryStats[currCatIndex].value += quantity;
+                else categoryStats.push({ label: category, value: quantity });
+            };
+
+            setLoading(true);
+
+            // If no cookie return error
+            if (!cookies.eShopToken) {
+                errorMsg('Please login and try again.');
+                return setLoading(false);
             }
 
             const res = await getHistory(cookies.eShopToken);
 
-            if(!res?.data) {
-                errorMsg('Something went wrong! Please try again.')
-            } else { // Add quantity to each product
-
-                res.data.forEach((order)=>order.products.forEach((product)=>{
-                    product.quantity=order.productIds.filter((currId) => currId == product._id).length
-                }))
-                setOrders(res.data)
+            if (!res?.data || !res.data.length) {
+                errorMsg('Something went wrong! Please try again.');
+            } else {
+                // Add quantity to each product and get category stats
+                res.data.forEach(order =>
+                    order.products.forEach(product => {
+                        (product.quantity = order.productIds.filter(
+                            currId => currId == product._id,
+                        ).length),
+                            addToCat(product.category, product.quantity);
+                    }),
+                );
+                setOrders(res.data);
+                setCategoryData(categoryStats);
             }
-            setLoading(false)
-          };
-        
-        fetchOrders()
-    },[]);
-   
+            setLoading(false);
+        };
+
+        fetchOrders();
+    }, []);
+
     return (
         <>
             <Navbar />
-            {loading ? <div className="spinner">
+            {loading ? (
+                <div className="spinner">
                     <div className="spinner-border" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </div>
-                </div> 
-                :
-                !orders.length ? <h1>It seems that you didn't make any orders yet</h1>
-                :
-                 <div>
-                    <h1>{userDetails.name}' orders</h1>
-
-                    {orders?.map((order)=> 
-                     <Accordion className='orderAccordion' key={order._id} expanded={expandedOrder==order._id} onChange={()=>setExpandedOrder(order._id)} >
-                        <AccordionSummary className="accordionTitle" expandIcon={<ExpandMoreIcon />}>
-                            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                                {new Date(parseInt(order.orderDate)).toLocaleDateString("en-US")}   
-                            </Typography>
-                            <Typography sx={{ color: 'text.secondary' }}>total: {order.totalPrice}</Typography>
-                            <Button className='goToOrderBtn'>
-                                <NavLink className="nav-link" to={`/order/${order._id}`}>
-                                  Go to order
-                                </NavLink>
-                            </Button>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            {order?.products?.map((product) => {
-                                const { image, name, description, rate, category, price, _id, quantity } = product;
-                                const props = { image, name, description, rate, category, price, _id, quantity};
-                                return <ProductOrder key={product._id} {...props}/>
-                            })}
+                </div>
+            ) : !orders.length ? (
+                <h1>It seems that you didn't make any orders yet</h1>
+            ) : (
+                <div>
+                    <h1>{userDetails.name}'s orders</h1>
+                    <CategoryPieChart data={categoryData} />
+                    {orders?.map(order => (
+                        <Accordion
+                            className="orderAccordion"
+                            key={order._id}
+                            expanded={expandedOrder == order._id}
+                            onChange={() => setExpandedOrder(order._id)}
+                        >
+                            <AccordionSummary
+                                className="accordionTitle"
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                <Typography
+                                    sx={{ width: '33%', flexShrink: 0 }}
+                                >
+                                    {new Date(
+                                        parseInt(order.orderDate),
+                                    ).toLocaleDateString('en-US')}
+                                </Typography>
+                                <Typography sx={{ color: 'text.secondary' }}>
+                                    total: {order.totalPrice}
+                                </Typography>
+                                <Button className="goToOrderBtn">
+                                    <NavLink
+                                        className="nav-link"
+                                        to={`/order/${order._id}`}
+                                    >
+                                        Go to order
+                                    </NavLink>
+                                </Button>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                {order?.products?.map(product => {
+                                    const {
+                                        image,
+                                        name,
+                                        description,
+                                        rate,
+                                        category,
+                                        price,
+                                        _id,
+                                        quantity,
+                                    } = product;
+                                    const props = {
+                                        image,
+                                        name,
+                                        description,
+                                        rate,
+                                        category,
+                                        price,
+                                        _id,
+                                        quantity,
+                                    };
+                                    return (
+                                        <ProductOrder
+                                            key={product._id}
+                                            {...props}
+                                        />
+                                    );
+                                })}
                             </AccordionDetails>
-                    </Accordion>)}
-                </div>}
-            
+                        </Accordion>
+                    ))}
+                </div>
+            )}
+
             <Footer />
         </>
     );
